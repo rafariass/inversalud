deaa8afa2926dd6fa561c50d011d4c9644abbff2
Raul Farias S <ra.farias.s@outlook.com>
2020-08-26 16:36:41 -0400

    Commit svg
    se quita el svg de la lupa porque no se ve bien.

--- source/php/constantes.php
+++ source/php/constantes.php
@@ -1,4 +1,12 @@
 <?php
-
-define('EMAIL_USER', 'inversalud.cl@gmail.com');
-define('EMAIL_PASSWORD', 'EL25ad2ser1.');
+    define('EMAIL_USER', 'inversalud.cl@gmail.com');
+    define('EMAIL_PASSWORD', 'EL25ad2ser1.');
+    // define('EMAIL_USER_TOMADEMUESTRAS', 'tomademuestras@inversalud.cl');
+    // define('EMAIL_PASSWORD_TOMADEMUESTRAS', '');
+    // define('EMAIL_USER_ECOTOMOGRAFIA', 'ecotomografia@inversalud.cl');
+    // define('EMAIL_PASSWORD_ECOTOMOGRAFIA', '');
+    // define('EMAIL_USER_CONTACTO', 'contacto@inversalud.cl');
+    // define('EMAIL_PASSWORD_CONTACTO', '');
+    // define('EMAIL_USER_PABELLON', 'pabellon@Inversalud.cl');
+    // define('EMAIL_PASSWORD_PABELLON', '');
+?>
